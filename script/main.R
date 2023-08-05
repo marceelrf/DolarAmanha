b0049bcfe98e1008756b153869c1da8295469793
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2023-08-05 10:54:45 -0300

    update 20230805

--- script/main.R
+++ script/main.R
@@ -18,6 +18,6 @@ Dates <- seq(from = Sys.Date(), to = (Sys.Date() - lookback + 1), by = -1)
 tibble(value = as.numeric(Dolar_360$bid),
        Date = Dates) %>% 
   ggplot(aes(x = Date, y = value)) +
-  geom_point(color = "black",size =2) +
-  geom_line(color = "red") +
+  geom_point(color = "black",size =2,alpha = .6) +
+  geom_line(color = "black") +
   labs(title = glue("{Sys.Date() - 360 + 1} - {Sys.Date()}"))
